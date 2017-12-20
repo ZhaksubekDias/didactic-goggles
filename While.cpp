c4eaf9550d99248f60099351fb14655c6cbe2a50
ZhaksubekDias <diaasssss8@gmail.com>
2017-12-20 14:22:35 +0600

    Made changes

--- While.cpp
+++ While.cpp
@@ -1,15 +1,20 @@
 #include "iostream"
+#include "string"
+
 using namespace std;
 
-int password, qwerty;
-int main(){
+int main()
+{
+	string password;
+	string key = "qwerty";
 	cout << "Enter the password" << endl;
 	cin >> password;
-	while(password!=qwerty){
+	while (password != key)
+	{
 		cout << "Incorrect password!" << endl << "Enter again" << endl;
 		cin >> password;
 	}
-	if (password==qwerty){
+	if (password == key)
+	{
 		cout << "WELCOME!" << endl;
-	} 
-}
+	}
