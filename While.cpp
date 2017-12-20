8c1751ec71276c99b10e8b6001d955f64b514de1
ZhaksubekDias <diaasssss8@gmail.com>
2017-12-20 13:40:37 +0600

    Add While.cpp

---
+++ While.cpp
@@ -0,0 +1,15 @@
+#include "iostream"
+using namespace std;
+
+int password, qwerty;
+int main(){
+	cout << "Enter the password" << endl;
+	cin >> password;
+	while(password!=qwerty){
+		cout << "Incorrect password" << endl << "Enter again" << endl;
+		cin >> password;
+	}
+	if (password==qwerty){
+		cout << "WELCOME!" << endl;
+	} 
+}
